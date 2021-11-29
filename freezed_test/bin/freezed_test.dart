0e4d0933d4f19a43dc1c18119bfa085504a7385d
trofimov <dev.mobile@yandex.ru>
2021-11-29 10:44:47 +0300

    add other class model

--- freezed_test/bin/freezed_test.dart
+++ freezed_test/bin/freezed_test.dart
@@ -1,3 +1,28 @@
+import 'model_1/user.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  User user1 = User(name: 'asd', age: 123);
+  User user2 = User(name: "asd", age: 123);
+
+  // true
+  print(user2 == user1);
+
+  // change user 2
+  User user3 = user2.copyWith(age: 3);
+
+  // equally
+  print(user3.toString());
+  print(user3);
+
+  // in json _>_ {name: asd, age: 123}
+  final json = user1.toJson();
+  print('user 1 to json $json');
+
+  // from json _>_ User(name: asd, age: 123)
+  User userFromJson = User.fromJson(json);
+  print('user 1 from json $userFromJson');
+
+  // custom method from model
+
+  print(user3.infoUser());
 }
